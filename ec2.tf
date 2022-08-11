b972377a015db3cc72864d8ef15fbb835d608c41
alex pattison <alexwpattison@gmail.com>
2022-08-10 17:31:00 -0700

    adding ec2 and provividers

---
+++ ec2.tf
@@ -0,0 +1,6 @@
+resource "aws_instance" "blerg" {
+  ami = "ami-090fa75af13c156b4"
+  instance_type = "t2.micro"
+  associate_public_ip_address = true
+  vpc_security_group_ids = [ "sg-04d863cace8378ff1" ]
+}
